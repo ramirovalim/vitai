fc26a85c833a7f416fd2d7c92e9da9fc4871a5f8
Ramiro Valim <ramirotuk@gmail.com>
2025-01-24 00:01:06 -0300

    setup: react-hook-forms, zod, shadcn/ui

--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -1,40 +1,64 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
-const SignUpSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(3).max(20),
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(2, { message: "Username must be at least 2 characters." })
+    .max(50, { message: "Username must be 50 characters maximum." }),
 });
 
-type SignUpSchemaType = z.infer<typeof SignUpSchema>;
+export function MyForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
 
-export default function RegisterForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<SignUpSchemaType>({ resolver: zodResolver(SignUpSchema) });
-
-  const onSubmit: SubmitHandler<SignUpSchemaType> = (data) => console.log(data);
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="form">
-      <input className="input" placeholder="email" {...register("email")} />
-      {errors.email && (
-        <span className="text-red-500">{errors.email.message}</span>
-      )}
-      <input
-        className="input"
-        placeholder="password"
-        {...register("password")}
-      />
-      {errors.password && (
-        <span className="text-red-500">{errors.password.message}</span>
-      )}
-
-      <button type="submit">submit!</button>
-    </form>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                {/* Your form field */}
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   );
 }
